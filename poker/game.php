984a4f98defc141020ed313ca1cf8626185acf87
Feke András <feke13@gmail.com>
2024-10-15 11:28:43 +0200

    Add initial files for poker game

---
+++ poker/game.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+include 'logic.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Póker Játék</title>
+</head>
+<body>
+    
+   
+</body>
+</html>
---
+++ poker/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Póker Játék</title>
+</head>
+<body>
+    <h1>Póker - Játék Robot Ellen</h1>
+    <a href="game.php">Játék Kezdése</a>
+</body>
+</html>
